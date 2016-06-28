ea5b40b9ef5ba82c22e04a46155cbfda5441e7d2
theor <theo.richart@gmail.com>
2016-06-28 14:45:15 -0400

    simple zstring extraction

--- src/ZString.hs
+++ src/ZString.hs
@@ -1,6 +1,9 @@
 module ZString where
 
+import qualified Data.ByteString as B
 import qualified Data.Binary.Strict.BitGet as BG
+import qualified Data.Binary.Strict.Get as S
+import Data.Word
 import Data.Either
 
 newtype Abbr = Abbr Integer
@@ -11,9 +14,22 @@ newtype ZstringAddress = Zstring Integer
 getAbbrWordAddress ::  Abbr -> WordZstringAddress
 getAbbrWordAddress (Abbr a) = WordZstring 0
 
+type Z3Pack = (Bool, [Word8])
+
+alphabetTable = " ?????abcdefghijklmnopqrstuvwxyz"
+
+decode :: BG.BitGet Z3Pack
 decode = do
   end <- BG.getBit
   c1 <- BG.getAsWord8 5
   c2 <- BG.getAsWord8 5
   c3 <- BG.getAsWord8 5
-  return (end,c1,c2,c3)
+  return (end,[c1,c2,c3])
+
+at = (Prelude.!!) alphabetTable . (\x -> fromIntegral x :: Int)
+-- decodeString s :: BG.BitGet [Word8]
+decodeString s =
+  case BG.runBitGet s decode of
+    Left a -> []
+    Right (True,c) -> map at c
+    Right (False,c) -> map at c ++ decodeString (B.drop 2 s)
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,20 +12,32 @@ import ZString
 
 main :: IO ()
 main = hspec $ do
-  describe "Decode zstring" $
-    it "should asd" $
+
+  describe "Decode zstring" $ do
+
+    it "should unpack zstring el" $
+      case BG.runBitGet (B.pack [0x65, 0xaa]) ZString.decode of
+        Left a -> expectationFailure a
+        Right a -> a `shouldBe` (False, [0x19 :: Word8, 0x0d :: Word8, 0x0a :: Word8])
+
+    it "should unpack zstring el" $
+      ZString.decodeString (B.pack [0x65, 0xaa]) `shouldBe` "the"
+    it "should unpack zstring el" $
+      ZString.decodeString (B.pack [0x65, 0xaa, 0x80, 0xa5]) `shouldBe` "the "
+    it "should unpack zstring" $
       case BG.runBitGet (B.pack [0b11010011,0b00100001]) ZString.decode of
         Left a -> expectationFailure a
-        Right a -> a `shouldBe` (True,0b10100 :: Word8, 0b11001 :: Word8, 0b1:: Word8)
+        Right a -> a `shouldBe` (True, [0b10100 :: Word8, 0b11001 :: Word8, 0b1:: Word8])
+
   describe "Prelude.head" $
     it "returns the first element of a list" $
       head [23 ..] `shouldBe` (23 :: Int)
+
   describe "Read Story Header" $
+
     it "should load file" $ do
       fi <- B.readFile "stories/minizork.z3"
       putStrLn "read"
       case fst $ S.runGet (readStory fi) fi of
         Left a -> expectationFailure a
         Right a -> print a
-
-        -- 1 `shouldBe` 1
